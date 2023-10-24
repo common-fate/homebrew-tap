201f269dc76d56de7bf2e558be3b50f1711ab4f5
Manish Chaulagain <manish23chaulagain@gmail.com>
2023-10-24 16:37:18 +1100

    update commonfate.rb

--- Casks/commonfate.rb
+++ Casks/commonfate.rb
@@ -1,13 +1,15 @@
 cask 'commonfate' do
   arch arm: "arm64", intel: "x64"
 
-  version "0.11.1"
+  version "0.10.0"
   
   on_arm do 
-    sha256 'ad6c2fe45d120c3b8103461aba1343f8d7a7d78c'
+    sha256 '1fc58e75a2b24b199f29a58356087959e9657b8ffe53038b6be6e4833d0a87a7'
+  end
 
   on_intel do
-    sha256 '1fc58e75a2b24b199f29a58356087959e9657b8f'
+    sha256 'ad6c2fe45d120c3b8103461aba1343f8d7a'
+  end
 
   url "https://releases.commonfate.io/desktop/stable/common-fate-#{arch}-#{version}.dmg"
 
