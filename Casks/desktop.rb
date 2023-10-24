e0381a733bebfe951b6507140f4e8b2d152fe5cb
runner <runner@Mac-1698115087618.local>
2023-10-24 02:39:50 +0000

    bumped version to '0.12.0' for intel mac

--- Casks/desktop.rb
+++ Casks/desktop.rb
@@ -6,7 +6,7 @@ cask 'desktop' do
     sha256 'ad6c2fe45d120c3b8103461aba1343f8d7a7d78c'
 
   on_intel do
-    version "0.11.0"
+    version "0.12.0"
     sha256 'ad6c2fe45d120c3b8103461aba1343f8d7a7d78c'
 
   url "https://releases.commonfate.io/desktop/stable/common-fate-#{arch}-#{version}.dmg"
