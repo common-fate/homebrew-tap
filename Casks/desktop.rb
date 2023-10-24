a3641570c5ac9bf5b9ac4edfc7acf84b5457096b
Eddie023 <Eddie023@users.noreply.github.com>
2023-10-24 05:02:12 +0000

    bumped version to 0.11.1

--- Casks/desktop.rb
+++ Casks/desktop.rb
@@ -1,13 +1,13 @@
 cask 'desktop' do
   arch arm: "arm64", intel: "x64"
 
-  version "0.11.0"
+  version "0.11.1"
   
   on_arm do 
     sha256 'ad6c2fe45d120c3b8103461aba1343f8d7a7d78c'
 
   on_intel do
-    sha256 'ad6c2fe45d120c3b8103461aba1343f8d7a7d78c'
+    sha256 '1fc58e75a2b24b199f29a58356087959e9657b8f'
 
   url "https://releases.commonfate.io/desktop/stable/common-fate-#{arch}-#{version}.dmg"
   sha256 'ad6c2fe45d120c3b8103461aba1343f8d7a7d78c'
