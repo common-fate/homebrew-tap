1f99769daddcc61b96306cb6d1711b06c46b5b02
Manish Chaulagain <manish23chaulagain@gmail.com>
2023-10-24 11:27:00 +1100

    Update desktop.rb

--- Casks/desktop.rb
+++ Casks/desktop.rb
@@ -1,5 +1,5 @@
 cask 'desktop' do
-  version "0.11.0"
+  version "0.10.0"
   sha256 'ad6c2fe45d120c3b8103461aba1343f8d7a7d78c'
 
   url "https://releases.commonfate.io/desktop/stable/common-fate-arm64-0.10.0.dmg"
