12c4ef8cb39a09f4fc217e21131796e8c1235a59
chrnorm <chrnorm@users.noreply.github.com>
2023-10-25 05:55:34 +0000

    bumped version to 0.10.0-canary.6636189522

--- Casks/common-fate.rb
+++ Casks/common-fate.rb
@@ -1,14 +1,14 @@
 cask 'common-fate' do
   arch arm: "arm64", intel: "x64"
 
-  version "0.10.0-canary.6636113909"
+  version "0.10.0-canary.6636189522"
   
   on_arm do 
-    sha256 'bea422cc8a7ff1e05ea31fc9ce093199a62b4a55'
+    sha256 '191b75dc7391008b6b0a8db57f193a2e214dcbbb'
   end
 
   on_intel do
-    sha256 '6caa7414636727dc7771cc39f12e8d16c1f4a4bc'
+    sha256 '5f28198f94ae66b153496c6b952427c4d8b2cce4'
   end
 
   url "https://releases.commonfate.io/desktop/stable/common-fate-#{arch}-#{version}.dmg"
